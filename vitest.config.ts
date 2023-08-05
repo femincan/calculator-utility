343c2201c891d54268c9571e1777c1ea66dd3911
Furkan Emin Can <femincanx@gmail.com>
2023-08-05 20:06:01 +0300

    test: add cobertura coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     include: ['src/**/test.ts'],
     coverage: {
       provider: 'v8',
-      reporter: 'text',
+      reporter: ['text', 'cobertura'],
     },
   },
 });
