cef2b73cc105161d610de6256e6e5ee035517624
Furkan Emin Can <femincanx@gmail.com>
2023-08-12 22:35:00 +0300

    test: add a new test for updated function

--- src/utils/isEndsWithOperator/test.ts
+++ src/utils/isEndsWithOperator/test.ts
@@ -1,6 +1,11 @@
 import { isEndsWithOperator } from '.';
 
 describe('isEndsWithOperator (Utility Function)', () => {
+  test('It should return false if the calculation string is empty', () => {
+    const check = isEndsWithOperator('');
+
+    expect(check).toBe(false);
+  });
   test('It should return true if there is an operator in the end of the calculation string', () => {
     const check = isEndsWithOperator('3+5-');
 
