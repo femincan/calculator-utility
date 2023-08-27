9034f9661798bd8b2278c7c85948813142a78ea8
Furkan Emin Can <femincanx@gmail.com>
2023-08-27 15:57:14 +0300

    test: update tests to compatible with changes

--- src/functions/getResult/test.ts
+++ src/functions/getResult/test.ts
@@ -14,11 +14,11 @@ describe('getResult (Function)', () => {
   test('If there is an operator end of the calculation string, it should removes it and returns the result', () => {
     const result = getResult('10+4-3*3+');
 
-    expect(result).toBe(5);
+    expect(result).toBe('5');
   });
   test('It should returns the result of the calculation string', () => {
     const result = getResult('37+2-2*3+6/2');
 
-    expect(result).toBe(36);
+    expect(result).toBe('36');
   });
 });
