f4e981898ca2e2a85291b5463570fef5ba488471
Furkan Emin Can <femincanx@gmail.com>
2023-08-27 08:27:22 +0300

    refactor: optimize addSeparator function

--- src/functions/addThousandsSeparator/wholeNumber.ts
+++ src/functions/addThousandsSeparator/wholeNumber.ts
@@ -1,5 +1,4 @@
 import { CalculationString } from '@src/types';
-import { removeCharactersFromEnd } from '@src/utils';
 
 const wholeNumberRegex = /^(.*\D{1})?(?<!\d+\.\d*)(\d+)$/;
 
@@ -7,18 +6,13 @@ const isEndsWithWholeNumber = (calculationString: CalculationString): boolean =>
   wholeNumberRegex.test(calculationString);
 
 const addSeparator = (wholeNumber: string): string => {
-  const reverseString = (string: string) =>
-    string.split('').toReversed().join('');
+  const wholeNumberArray = wholeNumber.split('');
 
-  const reversedWholeNumber = reverseString(wholeNumber);
-
-  const withSeparator = reversedWholeNumber.replaceAll(/(\d{3})/g, '$&,');
-
-  if (withSeparator.at(-1) === ',') {
-    return reverseString(removeCharactersFromEnd(withSeparator));
+  for (let i = wholeNumberArray.length - 3; i > 0; i -= 3) {
+    wholeNumberArray.splice(i, 0, ',');
   }
 
-  return reverseString(withSeparator);
+  return wholeNumberArray.join('');
 };
 
 export { wholeNumberRegex, isEndsWithWholeNumber, addSeparator };
