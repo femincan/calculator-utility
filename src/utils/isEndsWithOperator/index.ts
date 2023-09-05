46dd122afd4851a7867ce87d0d2a2e32218dc554
Furkan Emin Can <femincanx@gmail.com>
2023-09-05 09:05:24 +0300

    refactor: move operators constant to inside of isEndsWithOperator utility function

--- src/utils/isEndsWithOperator/index.ts
+++ src/utils/isEndsWithOperator/index.ts
@@ -1,7 +1,8 @@
-import { operators } from '@src/constants';
 import { CalculationString } from '@src/types';
 
 const isEndsWithOperator = (calculationString: CalculationString) => {
+  const operators = ['+', '-', '*', '/'];
+
   const lastCharacter = calculationString.at(-1);
 
   return lastCharacter ? operators.includes(lastCharacter) : false;
