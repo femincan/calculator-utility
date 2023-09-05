30d50480120bfb5b807237f677588997d634ecb4
Furkan Emin Can <femincanx@gmail.com>
2023-09-05 15:19:59 +0300

    test: refactor tests to compatible with changes

--- src/functions/getResult/test.ts
+++ src/functions/getResult/test.ts
@@ -8,13 +8,15 @@ describe('getResult (Function)', () => {
       errorString,
     );
   });
-  test('It should throw error if the calculation string is validated but unsupported for eval function', () => {
-    expect(() => getResult('24++')).toThrowError(errorString);
+  test('It should throw error if the calculation string is validated but it is unsupported for eval function', () => {
+    expect(() => getResult('24+++')).toThrowError(errorString);
   });
-  test('If there is an operator end of the calculation string, it should removes it and returns the result', () => {
-    const result = getResult('10+4-3*3+');
+  test('If there is one or two operators at the end of the calculation string, it should removes them and returns the result', () => {
+    const result1 = getResult('10+4-3*3+');
+    const result2 = getResult('2+2*-');
 
-    expect(result).toBe('5');
+    expect(result1).toBe('5');
+    expect(result2).toBe('4');
   });
   test('It should returns the result of the calculation string', () => {
     const result = getResult('37+2-2*3+6/2');
